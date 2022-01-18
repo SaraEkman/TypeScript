9254ac5b6171207030ef65d168d84cc27dc2d258
sara <sara.ekmanMedieinstitutet@gmail.com>
2022-01-18 10:26:47 +0100

    "update"

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+"use strict";
+window.onload = () => {
+    let name = 'Sara';
+    console.log(name);
+    function add(x, y) {
+        return x + y;
+    }
+    console.log(add(3, 6));
+};
---
+++ src/ts/main.ts
@@ -0,0 +1,9 @@
+window.onload = () => {
+  let name: String = 'Sara'
+  console.log(name)
+
+  function add(x: number, y: number): number {
+    return x + y
+  }
+  console.log(add(3, 6))
+}
